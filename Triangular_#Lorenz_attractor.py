0d445447c2c3cd2583956fe19c7b963bf12e8e2e
Gopal Dwivedi <gopal02112004@gmail.com>
2023-02-23 20:10:44 +0530

    Add files via upload

---
+++ Triangular_#Lorenz_attractor.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
+
+fig = plt.figure(figsize=plt.figaspect(0.5))
+# Make parameter spaces radii and angles.
+n_angles = 36
+n_radii = 8
+min_radius = 0.25
+radii = np.linspace(min_radius, 0.95, n_radii)
+
+angles = np.linspace(0, 2 * np.pi, n_angles, endpoint=False)
+angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
+angles[:, 1::2] += np.pi / n_angles
+
+# Map radius, angle pairs to x, y, z points.
+x = (radii * np.cos(angles)).flatten()
+y = (radii * np.sin(angles)).flatten()
+z = (np.cos(radii) * np.cos(3 * angles)).flatten()
+
+# Create the Triangulation; no triangles so Delaunay triangulation created.
+triang = mtri.Triangulation(x, y)
+
+# Mask off unwanted triangles.
+xmid = x[triang.triangles].mean(axis=1)
+ymid = y[triang.triangles].mean(axis=1)
+mask = xmid ** 2 + ymid ** 2 < min_radius ** 2
+triang.set_mask(mask)
+
+# Plot the surface.
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+ax.plot_trisurf(triang, z, cmap=plt.cm.CMRmap)
+
+
+def lorenz(xyz, *, s=10, r=28, b=2.667):
+    """
+    Parameters
+    ----------
+    xyz : array-like, shape (3,)
+       Point of interest in three-dimensional space.
+    s, r, b : float
+       Parameters defining the Lorenz attractor.
+
+    Returns
+    -------
+    xyz_dot : array, shape (3,)
+       Values of the Lorenz attractor's partial derivatives at *xyz*.
+    """
+    x, y, z = xyz
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
+    return np.array([x_dot, y_dot, z_dot])
+
+
+dt = 0.01
+num_steps = 10000
+
+xyzs = np.empty((num_steps + 1, 3))  # Need one more for the initial values
+xyzs[0] = (0., 1., 1.05)  # Set initial values
+# Step through "time", calculating the partial derivatives at the current point
+# and using them to estimate the next point
+for i in range(num_steps):
+    xyzs[i + 1] = xyzs[i] + lorenz(xyzs[i]) * dt
+
+# Plot
+ax = fig.add_subplot(1, 2, 2, projection='3d')
+
+ax.plot(*xyzs.T, lw=0.5)
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
+ax.set_title("Lorenz Attractor")
+
+plt.show()
